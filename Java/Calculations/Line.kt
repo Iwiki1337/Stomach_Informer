97ac791e7cb84311a05772adc452e2cbdb102b8d
Utro <utrotime@gmail.com>
2023-03-16 20:46:09 +0300

    Create Line class

---
+++ Java/Calculations/Line.kt
@@ -0,0 +1,6 @@
+package Calculations
+
+class Line(k : Float, b : Float) {
+    public var K : Float = k; //tan of angle between the line and the Ox axis
+    public var B : Float = b; //displacement of line
+}
