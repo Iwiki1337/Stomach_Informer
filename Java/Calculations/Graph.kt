7273ffde699b4daca92319eb920f57aeb4122a34
Utro <utrotime@gmail.com>
2023-03-17 00:06:32 +0300

    Fix language errors

--- Java/Calculations/Graph.kt
+++ Java/Calculations/Graph.kt
@@ -1,36 +1,31 @@
-import kotlin.collections
-
 package Calculations
-
 
 class Graph {
     public var Points : MutableList<Float> = mutableListOf<Float>();
 
-    constructor(var data: List<Float>){
+    constructor(data: List<Float>){
         for(value in data) {
             if(!Points.add(value)) throw Exception("Can't add point $value to the graph!");
         }
     }
 
-    public fun CalculateTrendLine(var depth : Uint) -> Line{
-        var s0 : Float = depth;
-        var s1 : Float = 0;
-        var s2 : Float = 0;
-        var t0 : Float = 0;
-        var t1 : Float = 0;
+    public fun CalculateTrendLine(depth : Int) : Line{
+        var s0 : Float = depth.toFloat();
+        var s1 : Float = 0.0f;
+        var s2 : Float = 0.0f;
+        var t0 : Float = 0.0f;
+        var t1 : Float = 0.0f;
 
         for (i in (Points.size - depth) until Points.size){
-            s1 += i;
-            s2 += i * i;
+            var t = i.toFloat();
+            s1 += t;
+            s2 += t * t;
             t0 += Points[i];
-            t1 += i * Points[i];
+            t1 += t * Points[i];
         }
 
-        var k : Float = 0;
-        var b : Float = 0;
-
-        k = (t1 * s0 - t0 * s1) / (s2 * s0 - s1 * s1);
-        b = (t0 - s1 * k) / so;
+        var k = (t1 * s0 - t0 * s1) / (s2 * s0 - s1 * s1);
+        var b = (t0 - s1 * k) / s0;
         return Line(k, b);
     }
 }
